6dcbbf256defca9dfa1598dcbcac79b8263e0d7d
PeggyYEEE <131346389+PeggyYEEE@users.noreply.github.com>
2023-04-23 11:05:27 +0800

    Delete myTheme.js

--- myTheme.js
+++
@@ -1,8 +0,0 @@
-function am4themes_myTheme(target) {
-    if (target instanceof am4core.ColorSet) {
-      target.list = [
-        am4core.color("#ff0077"),
-        am4core.color("#004216"),
-      ];
-    }
-  }
